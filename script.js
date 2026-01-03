0f3d5d27926a94fd74ef9718af14e270a4779315
Luke <141526416+Luke1183@users.noreply.github.com>
2026-01-04 00:29:41 +1100

    Refactor codebase into multiple event listeners

--- script.js
+++ script.js
@@ -30,6 +30,16 @@ function operate(a, b, operator) {
   }
 }
 
+function checkForInteger(number) {
+  if (number === undefined) {
+    return undefined;
+  } else if (num1.toString().split(".").length === 2) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
 function procNum(a) {
   aString = a.toString();
   aNum = Number(a);
@@ -52,91 +62,155 @@ function procNum(a) {
   return result;
 }
 
+function updateDisplay(num1, operator, num2) {
+  if (!(num1 === undefined)) {
+    num1Disp = procNum(num1);
+  }
+
+  if (!(num2 === undefined)) {
+    num2Disp = procNum(num2);
+  }
+
+  if (num1 === undefined && operator === undefined && num2 === undefined) {
+    return (calcDisplay.textContent = "0");
+  } else if (
+    !(num1 === undefined) &&
+    operator === undefined &&
+    num2 === undefined
+  ) {
+    return (calcDisplay.textContent = `${num1Disp}`);
+  } else if (
+    !(num1 === undefined) &&
+    !(operator === undefined) &&
+    num2 === undefined
+  ) {
+    if (operator === "+" || operator === "-") {
+      return (calcDisplay.textContent = `${num1Disp}   ${operator}`);
+    } else {
+      return (calcDisplay.textContent = `${num1Disp} ${operator}`);
+    }
+  } else if (
+    !(num1 === undefined) &&
+    !(operator === undefined) &&
+    !(num2 === undefined)
+  ) {
+    if (operator === "+" || operator === "-") {
+      return (calcDisplay.textContent = `${num1Disp}   ${operator}   ${num2Disp}`);
+    } else {
+      return (calcDisplay.textContent = `${num1Disp} ${operator} ${num2Disp}`);
+    }
+  }
+}
+
 let num1 = undefined;
 let num2 = undefined;
 let operator = undefined;
 let isResNum = false;
 let result = undefined;
 
-let num1IsInteger = true;
-let num2IsInteger = true;
+let num1IsInteger = undefined;
+let num2IsInteger = undefined;
 
 const calcDisplay = document.querySelector("#display");
-const buttons = document.querySelectorAll("button");
+const zeroButton = document.querySelector("#dig0");
+const digitButtons = document.querySelectorAll("button.dig");
+const decimalButton = document.querySelector("#dec");
+const operatorButtons = document.querySelectorAll("button.op");
+const minusButton = document.querySelector("#opNeg");
+const equalsButton = document.querySelector("#equals");
+const clearButton = document.querySelector("#clear");
+const deleteButton = document.querySelector("#delete");
 
 calcDisplay.textContent = "0";
 
-buttons.forEach((button) => {
+zeroButton.addEventListener("click", () => {
+  if (num1 === undefined) {
+    num1 = "0";
+  } else if (operator === undefined && !(num1 === undefined)) {
+    if (isResNum === true) {
+      num1 = "0";
+      isResNum = false;
+    } else {
+      num1 = num1 + "0";
+    }
+  } else if (!(operator === undefined) && num2 === undefined) {
+    num2 = "0";
+  } else {
+    num2 = num2 + "0";
+  }
+
+  num1IsInteger = checkForInteger(num1);
+  num2IsInteger = checkForInteger(num2);
+
+  updateDisplay(num1, operator, num2);
+});
+
+digitButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    if (
-      button.textContent === "0" ||
-      button.textContent === "1" ||
-      button.textContent === "2" ||
-      button.textContent === "3" ||
-      button.textContent === "4" ||
-      button.textContent === "5" ||
-      button.textContent === "6" ||
-      button.textContent === "7" ||
-      button.textContent === "8" ||
-      button.textContent === "9"
-    ) {
-      if (num1 === undefined) {
-        num1 = button.textContent;
-      } else if (operator === undefined && !(num1 === undefined)) {
-        if (isResNum === true) {
-          num1 = button.textContent;
-          isResNum = false;
-        } else {
-          num1 = num1 + button.textContent;
-        }
-      } else if (!(operator === undefined) && num2 === undefined) {
-        num2 = button.textContent;
-      } else {
-        num2 = num2 + button.textContent;
-      }
-    }
-
-    if (
-      button.textContent === "." &&
-      num1 === undefined &&
-      operator === undefined &&
-      num2 === undefined
-    ) {
-      num1 = "0" + button.textContent;
-    } else if (
-      button.textContent === "." &&
-      !(num1 === undefined) &&
-      operator === undefined &&
-      num2 === undefined
-    ) {
+    if (num1 === undefined) {
+      num1 = button.textContent;
+    } else if (operator === undefined && !(num1 === undefined)) {
       if (isResNum === true) {
-        num1 = "0" + button.textContent;
+        num1 = button.textContent;
         isResNum = false;
-      } else if (num1IsInteger === true) {
+      } else {
         num1 = num1 + button.textContent;
-      } else if (num1 === "-") {
-        num1 = num1 + "0" + button.textContent;
       }
-    } else if (
-      button.textContent === "." &&
-      !(num1 === undefined) &&
-      !(operator === undefined) &&
-      num2 === undefined
-    ) {
-      num2 = "0" + button.textContent;
-    } else if (
-      button.textContent === "." &&
-      !(num1 === undefined) &&
-      !(operator === undefined) &&
-      !(num2 === undefined)
-    ) {
-      if (Number.isInteger(Number(num2))) {
-        num2 = num2 + button.textContent;
-      } else if (num2 === "-") {
-        num2 = num2 + "0" + button.textContent;
-      }
+    } else if (!(operator === undefined) && num2 === undefined) {
+      num2 = button.textContent;
+    } else {
+      num2 = num2 + button.textContent;
     }
 
+    num1IsInteger = checkForInteger(num1);
+    num2IsInteger = checkForInteger(num2);
+
+    updateDisplay(num1, operator, num2);
+  });
+});
+
+decimalButton.addEventListener("click", () => {
+  if (num1 === undefined && operator === undefined && num2 === undefined) {
+    num1 = "0.";
+  } else if (
+    !(num1 === undefined) &&
+    operator === undefined &&
+    num2 === undefined
+  ) {
+    if (isResNum === true) {
+      num1 = "0.";
+      isResNum = false;
+    } else if (num1IsInteger === true) {
+      num1 = num1 + ".";
+    } else if (num1 === "-") {
+      num1 = num1 + "0.";
+    }
+  } else if (
+    !(num1 === undefined) &&
+    !(operator === undefined) &&
+    num2 === undefined
+  ) {
+    num2 = "0.";
+  } else if (
+    !(num1 === undefined) &&
+    !(operator === undefined) &&
+    !(num2 === undefined)
+  ) {
+    if (Number.isInteger(Number(num2))) {
+      num2 = num2 + ".";
+    } else if (num2 === "-") {
+      num2 = num2 + "0.";
+    }
+  }
+
+  num1IsInteger = checkForInteger(num1);
+  num2IsInteger = checkForInteger(num2);
+
+  updateDisplay(num1, operator, num2);
+});
+
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", () => {
     if (
       (button.textContent === "+" ||
         button.textContent === "-" ||
@@ -189,99 +263,128 @@ buttons.forEach((button) => {
       }
     }
 
-    if (
-      button.textContent === "=" &&
-      !(num1 === undefined) &&
-      !(operator === undefined) &&
-      num2 === undefined
-    ) {
-      operator = undefined;
-    } else if (
-      button.textContent === "=" &&
-      !(num1 === undefined) &&
-      !(operator === undefined) &&
-      !(num2 === undefined)
-    ) {
-      if (num2 === "-") {
-        operator = undefined;
-        num2 = undefined;
-      } else {
-        num1 = operate(Number(num1), Number(num2), operator);
-        operator = undefined;
-        num2 = undefined;
-        isResNum = true;
-      }
-    }
+    num1IsInteger = checkForInteger(num1);
+    num2IsInteger = checkForInteger(num2);
 
-    if (button.textContent === "Clear") {
-      num1 = undefined;
-      isResNum = false;
+    updateDisplay(num1, operator, num2);
+  });
+});
+
+minusButton.addEventListener("click", () => {
+  if (
+    (button.textContent === "+" ||
+      button.textContent === "-" ||
+      button.textContent === "*" ||
+      button.textContent === "/") &&
+    num1 === undefined &&
+    operator === undefined &&
+    num2 === undefined
+  ) {
+    if (button.textContent === "-") {
+      num1 = button.textContent;
+    }
+  } else if (
+    (button.textContent === "+" ||
+      button.textContent === "-" ||
+      button.textContent === "*" ||
+      button.textContent === "/") &&
+    !(num1 === undefined) &&
+    operator === undefined &&
+    num2 === undefined
+  ) {
+    if (!(num1 === "-")) {
+      operator = button.textContent;
+    }
+  } else if (
+    (button.textContent === "+" ||
+      button.textContent === "-" ||
+      button.textContent === "*" ||
+      button.textContent === "/") &&
+    !(num1 === undefined) &&
+    !(operator === undefined) &&
+    num2 === undefined
+  ) {
+    if (button.textContent === "-") {
+      num2 = button.textContent;
+    }
+  } else if (
+    (button.textContent === "+" ||
+      button.textContent === "-" ||
+      button.textContent === "*" ||
+      button.textContent === "/") &&
+    !(num1 === undefined) &&
+    !(operator === undefined) &&
+    !(num2 === undefined)
+  ) {
+    if (!(num2 === "-")) {
+      num1 = operate(Number(num1), Number(num2), operator);
+      operator = button.textContent;
+      num2 = undefined;
+    }
+  }
+
+  num1IsInteger = checkForInteger(num1);
+  num2IsInteger = checkForInteger(num2);
+
+  updateDisplay(num1, operator, num2);
+});
+
+equalsButton.addEventListener("click", () => {
+  if (
+    !(num1 === undefined) &&
+    !(operator === undefined) &&
+    num2 === undefined
+  ) {
+    operator = undefined;
+  } else if (
+    !(num1 === undefined) &&
+    !(operator === undefined) &&
+    !(num2 === undefined)
+  ) {
+    if (num2 === "-") {
       operator = undefined;
       num2 = undefined;
-      result = undefined;
-    }
-
-    if (button.textContent === "Delete") {
-      if (
-        !(num1 === undefined) &&
-        !(operator === undefined) &&
-        !(num2 === undefined)
-      ) {
-        num2 = num2.slice(0, num2.length - 1);
-        if (num2 === "") {
-          num2 = undefined;
-        }
-      }
-    }
-
-    if (num1.toString().split(".").length === 2) {
-      num1IsInteger = false;
     } else {
-      num1IsInteger = true;
+      num1 = operate(Number(num1), Number(num2), operator);
+      operator = undefined;
+      num2 = undefined;
+      isResNum = true;
     }
+  }
 
-    if (num2.toString().split(".").length === 2) {
-      num2IsInteger = false;
-    } else {
-      num2IsInteger = true;
-    }
+  num1IsInteger = checkForInteger(num1);
+  num2IsInteger = checkForInteger(num2);
 
-    if (!(num1 === undefined)) {
-      num1Disp = procNum(num1);
-    }
+  updateDisplay(num1, operator, num2);
+});
 
-    if (!(num2 === undefined)) {
-      num2Disp = procNum(num2);
-    }
+clearButton.addEventListener("click", () => {
+  num1 = undefined;
+  isResNum = false;
+  operator = undefined;
+  num2 = undefined;
+  result = undefined;
 
-    if (num1 === undefined && operator === undefined && num2 === undefined) {
-      calcDisplay.textContent = "0";
-    } else if (
-      !(num1 === undefined) &&
-      operator === undefined &&
-      num2 === undefined
-    ) {
-      calcDisplay.textContent = `${num1Disp}`;
-    } else if (
-      !(num1 === undefined) &&
-      !(operator === undefined) &&
-      num2 === undefined
-    ) {
-      if (operator === "+" || operator === "-") {
-        calcDisplay.textContent = `${num1Disp}   ${operator}`;
-      } else {
-        calcDisplay.textContent = `${num1Disp} ${operator}`;
-      }
-    } else if (
-      !(num1 === undefined) &&
-      !(operator === undefined) &&
-      !(num2 === undefined)
-    ) {
-      if (operator === "+" || operator === "-") {
-        calcDisplay.textContent = `${num1Disp}   ${operator}   ${num2Disp}`;
-      } else {
-        calcDisplay.textContent = `${num1Disp} ${operator} ${num2Disp}`;
-      }
+  num1IsInteger = undefined;
+  num2IsInteger = undefined;
+
+  updateDisplay(num1, operator, num2);
+});
+
+deleteButton.addEventListener("click", () => {
+  if (
+    !(num1 === undefined) &&
+    !(operator === undefined) &&
+    !(num2 === undefined)
+  ) {
+    num2 = num2.slice(0, num2.length - 1);
+    if (num2 === "") {
+      num2 = undefined;
     }
-  });
+  }
+
+  num1IsInteger = checkForInteger(num1);
+  num2IsInteger = checkForInteger(num2);
+
+  updateDisplay(num1, operator, num2);
 });
